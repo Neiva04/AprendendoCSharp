e8c774ab204b3532100a8f2d60fbf9f837862b20
Neiva04 <neivajoseph@gmail.com>
2023-03-21 07:58:23 -0300

    Atividade LP3

---
+++ LP3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Program {
+    public static void Main() {
+        Animal Koalsky = new Penguin();
+        Animal Bald = new Eagle();
+        Console.WriteLine(Koalsky);
+        Console.WriteLine(Bald);
+        ((Eagle)Bald).Fly();
+    }
+}
+
+public abstract class Animal {
+    public void Eat() {
+        Console.WriteLine("Hungry");
+    }
+}
+
+public interface IMoveable {
+    void Move();
+}
+
+public interface IFlyable {
+    void Fly();
+}
+
+public interface ICanEatMeat {
+    bool EatMeat { get; set; }
+}
+
+public interface ICanEatVegies {
+    bool EatVegies { get; set; }
+}
+
+public interface ISwimmable {
+    void Swim();
+}
+
+public interface IRunnable {
+    void Run();
+}
+
+public class Penguin : Animal, IMoveable, ICanEatMeat, ISwimmable {
+    public void Move() {
+        Console.WriteLine("I am movement able");
+    }
+
+    public bool EatMeat { get; set; } = true;
+
+    public void Swim() {
+        Console.WriteLine("I am swimming");
+    }
+}
+
+public class Eagle : Animal, IMoveable, ICanEatMeat, IFlyable {
+    public void Move() {
+        Console.WriteLine("I am movement able");
+    }
+
+    public bool EatMeat { get; set; } = true;
+
+    public void Fly() {
+        Console.WriteLine("I believe I can fly");
+    }
+}
